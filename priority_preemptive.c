e4e4eaa8e37cf893305e1f1e718853e0374d962f
srupat <srujanpatwardhan@gmail.com>
2023-09-18 10:17:44 +0530

    priority preeemptive

--- priority_preemptive.c
+++ priority_preemptive.c
@@ -2,11 +2,31 @@
 #include<stdlib.h>
 #include<stdbool.h>
 
-void find_waiting_time(int processes[], int n, int burst_time[], int waiting_time[], int arrival_time[]) {
+void sortAccordingToPriority(int arrival_time[], int priority[], int n)
+{
+    int temp;
+    for(int i = 0; i < n - 1; i++)
+    {
+        for(int j = 0; j < n - i - 1; j++)
+        {
+            if(priority[j] > priority[j + 1]) 
+            {
+                temp = arrival_time[j];
+                arrival_time[j] = arrival_time[j + 1];
+                arrival_time[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void find_waiting_time(int processes[], int n, int burst_time[], int waiting_time[], int arrival_time[],int priority[]) {
     int *remaining_time = (int *)malloc(n * sizeof(int));
     bool *completed = (bool *)malloc(n * sizeof(bool));
 
-    for (int i = 0; i < n; i++) {
+    sortAccordingToPriority(arrival_time,priority,n);
+
+    for (int i = 0; i < n; i++) 
+    {
         remaining_time[i] = burst_time[i];
         completed[i] = false;
     }
@@ -55,21 +75,22 @@ void find_turnaround_time(int processes[], int n, int burst_time[], int waiting_
     }
 }
 
-void printTable(int processes[], int arrival_time[], int n, int burst_time[]) {
+void printTable(int processes[], int arrival_time[], int n, int burst_time[], int priority[]) {
     int total_waiting_time = 0, total_turnaround_time = 0;
     int *waiting_time = (int *)malloc(n * sizeof(int));
     int *turnaround_time = (int *)malloc(n * sizeof(int));
 
-    find_waiting_time(processes, n, burst_time, waiting_time, arrival_time);
+    find_waiting_time(processes, n, burst_time, waiting_time, arrival_time,priority);
     find_turnaround_time(processes, n, burst_time, waiting_time, turnaround_time);
 
-    printf("PID\tAT\tBT\tWT\tTAT\n");
+    printf("PID\tPr\tAT\tBT\tWT\tTAT\n");
 
     for (int i = 0; i < n; i++) {
         total_waiting_time += waiting_time[i];
         total_turnaround_time += turnaround_time[i];
 
         printf("%d\t", i + 1);
+        printf("%d\t",priority[i]);
         printf("%d\t", arrival_time[i]);
         printf("%d\t", burst_time[i]);
         printf("%d\t", waiting_time[i]);
@@ -85,10 +106,11 @@ void printTable(int processes[], int arrival_time[], int n, int burst_time[]) {
 int main() {
     int processes[] = {1, 2, 3, 4};
     int arrival_time[] = {1, 8, 5, 9};
+    int priority[] = {3,4,1,2};
     int n = sizeof(processes) / sizeof(int);
     int burst_time[] = {20, 3, 5, 10};
 
-    printTable(processes, arrival_time, n, burst_time);
+    printTable(processes, arrival_time, n, burst_time, priority);
 
     return 0;
 }
