2976b2eda0a1685804a00fdccf024d39d7bdab3d
srupat <srujanpatwardhan@gmail.com>
2024-01-22 10:16:20 +0530

    remaining

--- scan_disk.c
+++ scan_disk.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main()
+{
     int n, head, total_seek_time = 0;
 
     printf("Enter the number of disk requests: ");
     scanf("%d", &n);
 
-    int *request_queue= (int*)malloc(sizeof(int)*n);
+    int *request_queue = (int *)malloc(sizeof(int) * n);
 
     printf("Enter the disk requests:\n");
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         scanf("%d", &request_queue[i]);
     }
 
@@ -23,10 +25,14 @@ int main() {
     printf("\nEnter the direction of head movement (0 for left, 1 for right): ");
     scanf("%d", &direction);
 
-    if (direction == 0) {
-        for (int i = head; i >= 0; i--) {
-            for (int j = 0; j < n; j++) {
-                if (request_queue[j] == i) {
+    if (direction == 0)
+    {
+        for (int i = head; i >= 0; i--)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (request_queue[j] == i)
+                {
                     total_seek_time += abs(head - request_queue[j]);
                     head = request_queue[j];
                     printf(" -> %d", head);
@@ -36,17 +42,24 @@ int main() {
         total_seek_time += head;
         head = 0;
         printf(" -> 0");
-        for (int i = 0; i < n; i++) {
-            if (request_queue[i] > 0) {
+        for (int i = 0; i < n; i++)
+        {
+            if (request_queue[i] > 0)
+            {
                 total_seek_time += abs(head - request_queue[i]);
                 head = request_queue[i];
                 printf(" -> %d", head);
             }
         }
-    } else if (direction == 1) {
-        for (int i = head; i <= 199; i++) {
-            for (int j = 0; j < n; j++) {
-                if (request_queue[j] == i) {
+    }
+    else if (direction == 1)
+    {
+        for (int i = head; i <= 199; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (request_queue[j] == i)
+                {
                     total_seek_time += abs(head - request_queue[j]);
                     head = request_queue[j];
                     printf(" -> %d", head);
@@ -56,8 +69,10 @@ int main() {
         total_seek_time += 199 - head;
         head = 199;
         printf(" -> 199");
-        for (int i = 0; i < n; i++) {
-            if (request_queue[i] < 199) {
+        for (int i = 0; i < n; i++)
+        {
+            if (request_queue[i] < 199)
+            {
                 total_seek_time += abs(head - request_queue[i]);
                 head = request_queue[i];
                 printf(" -> %d", head);
